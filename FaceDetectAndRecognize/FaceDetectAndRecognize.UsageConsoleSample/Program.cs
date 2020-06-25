e1bf947d7ec3adcfeeb1ad12fb042fe05e45d9d1
nguyen phan du <badpaybad@gmail.com>
2020-06-25 19:24:31 +0700

    sample convert gif to mp4

---
+++ FaceDetectAndRecognize/FaceDetectAndRecognize.UsageConsoleSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using FaceDetectAndRecognize.Core;
+using System;
+using System.IO;
+
+namespace FaceDetectAndRecognize.UsageConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+           var ffmpegConvertToVid= new FfmpegCommandBuilder()
+                .WithInputFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sampledata/intro.gif"), 5)
+                .WithOutput(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sampledata/intro.mp4"))
+                .WithOutDuration(5)
+                .ToCommand().Run();
+
+            Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(ffmpegConvertToVid));
+
+            Console.ReadLine();
+        }
+    }
+}
