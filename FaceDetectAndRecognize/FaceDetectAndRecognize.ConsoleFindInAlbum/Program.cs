5c1afabdcb97e8eb67539ad0c43fe42ff2e7e799
nguyen phan du <badpaybad@gmail.com>
2020-06-27 17:43:16 +0700

    check if album and face folder have files

--- FaceDetectAndRecognize/FaceDetectAndRecognize.ConsoleFindInAlbum/Program.cs
+++ FaceDetectAndRecognize/FaceDetectAndRecognize.ConsoleFindInAlbum/Program.cs
@@ -18,40 +18,45 @@ namespace FaceDetectAndRecognize.ConsoleFindInAlbum
 
             var rootIds = Directory.GetDirectories(dirRoot);
 
-            foreach(var rp in rootIds)
+            foreach (var rp in rootIds)
             {
                 var rid = GetIdByPath(rp);
                 var identityDir = Directory.GetDirectories(Path.Combine(dirRoot, rid.ToString()));
-                foreach(var ip in identityDir)
+                foreach (var ip in identityDir)
                 {
                     var iid = GetIdByPath(ip);
                     var folderFace = Path.Combine(dirRoot, $"{rid}/{iid}/face");
                     var folderAlbum = Path.Combine(dirRoot, $"{rid}/{iid}/album");
                     var folderResult = Path.Combine(dirRoot, $"{rid}/{iid}/result");
-                    
+
                     CreateDirIfNotExist(folderAlbum, folderFace, folderResult);
 
                     var listFace = Directory.GetFiles(folderFace);
                     var listPhoto = Directory.GetFiles(folderAlbum);
 
+                    if (listFace.Length == 0 || listPhoto.Length == 0)
+                    {
+                        continue;
+                    }
+
                     var faceRekognize = new FaceRecognition(iid);
 
-                    foreach(var ff in listFace)
+                    foreach (var ff in listFace)
                     {
                         faceRekognize.AddTrainData(new Image<Bgr, byte>(ff));
                     }
                     faceRekognize.WithThreshold(3000, 44);
                     faceRekognize.Train();
 
-                    foreach(var photo in listPhoto)
+                    foreach (var photo in listPhoto)
                     {
                         Image<Bgr, byte> pOrigin = new Image<Bgr, byte>(photo);
                         var pInfo = new FileInfo(photo);
-                        var result= faceRekognize.RecognizePhoto(pOrigin);
+                        var result = faceRekognize.RecognizePhoto(pOrigin);
                         if (result.Count > 0)
                         {
                             string dirFaceOfPhoto = Path.Combine(folderResult, pInfo.Name);
-                         
+
                             CreateDirIfNotExist(dirFaceOfPhoto);
 
                             foreach (var r in result)
@@ -75,9 +80,9 @@ namespace FaceDetectAndRecognize.ConsoleFindInAlbum
             return int.Parse(arr[arr.Length - 1]);
         }
 
-        static void CreateDirIfNotExist(params string [] dirPaths)
+        static void CreateDirIfNotExist(params string[] dirPaths)
         {
-            foreach(var d in dirPaths)
+            foreach (var d in dirPaths)
             {
                 if (Directory.Exists(d) == false) Directory.CreateDirectory(d);
             }
